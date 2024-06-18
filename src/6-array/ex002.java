f9a888981b4ec3b614df6486eb9c65e07d560a9b
FoythCaue <foythcaue@gmail.com>
2024-06-17 21:24:08 -0300

    ajustes array

--- src/6-array/ex002.java
+++ src/6-array/ex002.java
@@ -1,23 +1,27 @@
 import javax.swing.JOptionPane;
 
-//Não terminar o exercício
+import java.text.DecimalFormat;
 
 public class ex002 {
     public static void main(String[] args) {
         int[] firstGrade = new int[5];
         int[] secondGrade = new int[5];
         int[] thirdGrade = new int[5];
-        int[] averageGrade = new int[5];
-        int wheightGrade = 0;
+        double[] averageGrade = new double[5];
+
+        DecimalFormat df = new DecimalFormat();
+        df.applyPattern("0.00");
 
         for (int i = 0; i < 5; i++) {
-            firstGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Primeira nota: ", "Notas", JOptionPane.QUESTION_MESSAGE));
-            secondGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Segunda nota: ", "Notas", JOptionPane.QUESTION_MESSAGE));
-            thirdGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Terceira nota: ", "Notas", JOptionPane.QUESTION_MESSAGE));
-            wheightGrade = Integer.parseInt(JOptionPane.showInputDialog(null, "Peso da "+i+1+"° nota: ", "Notas", JOptionPane.QUESTION_MESSAGE));
+            firstGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Primeira nota do aluno " + (i+1) + ": ", "Notas", JOptionPane.QUESTION_MESSAGE));
+            secondGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Segunda nota do aluno " + (i+1) + ": ", "Notas", JOptionPane.QUESTION_MESSAGE));
+            thirdGrade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Terceira nota do aluno " + (i+1) + ": ", "Notas", JOptionPane.QUESTION_MESSAGE));
 
-            averageGrade[i] = (firstGrade[i] + secondGrade[i] + thirdGrade[i]) / 3;
-            JOptionPane.showMessageDialog(null, "Média: " + averageGrade[i], "Notas", JOptionPane.INFORMATION_MESSAGE);
+            averageGrade[i] = ((firstGrade[i]*0.2) + (secondGrade[i]*0.3) + (thirdGrade[i]*0.5)) / (0.2 + 0.3 + 0.5);
         }
+        for (int i = 0; i < averageGrade.length; i++) {
+            JOptionPane.showMessageDialog(null, "Notas: " + firstGrade[i] + " " + secondGrade[i] + " " + thirdGrade[i] + " | Média do aluno " + (i+1) + ": " + df.format(averageGrade[i]), "Notas", JOptionPane.INFORMATION_MESSAGE);
+        }
+        
     }
 }
